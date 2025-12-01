51d0591c0b52449db43cd7b18598641dab6791d6
annalyngrc-ctrl <annalyngrc@gmail.com>
2025-12-01 03:13:20 +0000

    Updated urls to include octofit_app routes

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -1,23 +1,14 @@
-"""octofit_tracker URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
+import os
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from . import views
+
+codespace_name = os.environ.get('CODESPACE_NAME')
+if codespace_name:
+    base_url = f"https://{codespace_name}-8000.app.github.dev"
+else:
+    base_url = "http://localhost:8000"
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -28,12 +19,9 @@ router.register(r'leaderboard', views.LeaderboardViewSet)
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 
 @api_view(['GET'])
 def api_root(request, format=None):
-    # Dynamically build the base URL using the request host (supports codespace and localhost)
-    base_url = f"{request.scheme}://{request.get_host()}"
     return Response({
         'users': f"{base_url}/api/users/",
         'teams': f"{base_url}/api/teams/",
@@ -44,6 +32,6 @@ def api_root(request, format=None):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', api_root, name='api-root'),
     path('api/', include(router.urls)),
-    path('', api_root, name='api-root'),
 ]
